5165d6c510bee095d008f99371f8f808607f84eb
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-23 22:38:14 -0300

    style: global css config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading attr='property'>
-        Olá, mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, similique
-        corporis quaerat natus culpa est veniam repellendus reiciendis alias.
-        Quo unde architecto optio quos magni atque necessitatibus, est aliquam
-        voluptate.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
