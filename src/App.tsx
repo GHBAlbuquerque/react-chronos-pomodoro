df37461ba23ef7cb8688a23413b210d69dcf6450
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-24 21:25:06 -0300

    feature: creates menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+//import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
@@ -11,7 +12,11 @@ export function App() {
       <Container>
         <Logo />
       </Container>
-      <Container>MENU</Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
       <Container>FORM</Container>
       <Container>FOOTER</Container>
     </>
