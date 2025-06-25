a4bdf57295e5904e0c3a64f5c51177d62b38bff8
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-25 17:15:43 -0300

    feature: light/dark mode change impl - WIP

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,55 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
+
+type AvailableModes = 'dark' | 'light';
 
 export function Menu() {
+  const [mode, changeMode] = useState<AvailableModes>('dark');
+
+  function handleModeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
     <div className={styles.menu}>
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Homepage'
+        title='Homepage'
+      >
         <HouseIcon />
       </a>
 
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='History'
+        title='History'
+      >
         <HistoryIcon />
       </a>
 
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Settings'
+        title='Settings'
+      >
         <SettingsIcon />
       </a>
 
-      <a href='#' className={styles.menuLink}>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Change mode'
+        title='Change mode'
+        onClick={handleModeChange}
+      >
         <SunIcon />
       </a>
     </div>
