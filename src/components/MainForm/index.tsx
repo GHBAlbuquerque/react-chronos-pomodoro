92c1736f2122ec2dcfcba29feffcbb58bc0eb345
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-26 20:18:38 -0300

    example: using useRef to capture values without re-rendering components

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,28 @@ import { PlayCircleIcon } from 'lucide-react';
 import { DefaultButton } from '../Button';
 import { Cycles } from '../Cycles';
 import { DefaultInput } from '../DefaultInput';
+import { useRef, useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(taskName, taskNameInput.current.value);
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           type='text'
           id='input'
           labelText='task'
           placeholder='Write a task'
+          // value={taskName}
+          // onChange={e => setTaskName(e.target.value)}
+          ref={taskNameInput}
         />
       </div>
 
