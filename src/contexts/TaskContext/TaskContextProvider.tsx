5bed47a509224d6e8015801ca1f588428ec401a8
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-27 23:33:55 -0300

    feature: configures worker to do countdown - only console.logging

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -14,10 +14,24 @@ export function TaskContextProvider({ children }: TaskContentProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
   const worker = TimerWorkerManager.getInstance();
 
+  worker.onmessage(e => {
+    const countDownSeconds = e.data; // event.data here will be the value sent from the worker
+    console.log(countDownSeconds); // This will log countDownSeconds
+
+    if (countDownSeconds == 0) {
+      console.log('Work completed! :-)');
+      worker.terminate();
+    }
+  });
+
   useEffect(() => {
-    console.log(state);
-    console.log(worker);
-  }, [state]);
+    if (!state.activeTask) {
+      worker.terminate();
+      console.log('Worker stopped because there is no active task.');
+    }
+
+    worker.postMessage(state);
+  }, [state, worker]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
