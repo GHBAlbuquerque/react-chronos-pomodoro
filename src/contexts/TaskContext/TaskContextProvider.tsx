8828b3484f1b5d14bb03ee853ee06ff79bdcd18d
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-28 20:28:54 -0300

    feature: impl sound beep when countdown is done

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from '.';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWokerManager';
 import { TaskActionTypes } from './taskAction';
+import { loadBeep } from '../../utils/loadBeep';
 
 // Context Provider & Props
 
@@ -14,12 +15,16 @@ type TaskContentProviderProps = {
 export function TaskContextProvider({ children }: TaskContentProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
   const worker = TimerWorkerManager.getInstance();
+  const playBeepRef = useRef<() => void | null>(null);
 
   worker.onmessage(e => {
     const countDownSeconds = e.data; // event.data here will be the value sent from the worker
-    console.log(countDownSeconds); // This will log countDownSeconds
 
-    if (countDownSeconds == 0) {
+    if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+      }
+
       dispatch({
         type: TaskActionTypes.COMPLETE_TASK,
       });
@@ -40,6 +45,14 @@ export function TaskContextProvider({ children }: TaskContentProviderProps) {
 
     worker.postMessage(state);
   }, [state, worker]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep(); //loads audio
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
