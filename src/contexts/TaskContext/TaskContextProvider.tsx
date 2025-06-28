787e373061c870f2266c7d96212537c7b678b927
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-27 22:56:24 -0300

    feature: creates singleton TimerWorkerManager for countdown management

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from '.';
 import { taskReducer } from './taskReducer';
+import { TimerWorkerManager } from '../../workers/TimerWokerManager';
 
 // Context Provider & Props
 
@@ -11,9 +12,11 @@ type TaskContentProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContentProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const worker = TimerWorkerManager.getInstance();
 
   useEffect(() => {
     console.log(state);
+    console.log(worker);
   }, [state]);
 
   return (
