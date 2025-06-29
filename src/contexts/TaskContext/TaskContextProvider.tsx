fb0e2b4a712c85327ebdb0e6bbd449787ba40125
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-28 23:33:53 -0300

    feature: saves current state on local storage and retrieves it when back

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,6 +5,7 @@ import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWokerManager';
 import { TaskActionTypes } from './taskAction';
 import { loadBeep } from '../../utils/loadBeep';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
 // Context Provider & Props
 
@@ -13,7 +14,19 @@ type TaskContentProviderProps = {
 };
 
 export function TaskContextProvider({ children }: TaskContentProviderProps) {
-  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState, () => {
+    const storageState = localStorage.getItem('state');
+
+    if (!storageState) return initialTaskState;
+    const parsedState: TaskStateModel = JSON.parse(storageState);
+
+    return {
+      ...parsedState,
+      activeTask: null,
+      secondsRemaining: 0,
+      formattedSecondsRemaining: '00:00',
+    };
+  });
   const worker = TimerWorkerManager.getInstance();
   const playBeepRef = useRef<() => void | null>(null);
 
@@ -38,6 +51,8 @@ export function TaskContextProvider({ children }: TaskContentProviderProps) {
   });
 
   useEffect(() => {
+    localStorage.setItem('state', JSON.stringify(state));
+
     if (!state.activeTask) {
       worker.terminate();
       console.log('Worker stopped because there is no active task.');
