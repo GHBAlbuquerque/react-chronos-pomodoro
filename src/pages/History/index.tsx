169aaa06042b7ef5cb48c7acc49af72a400f083e
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-29 00:35:05 -0300

    feature: impl history table - WIP

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -5,8 +5,13 @@ import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
 
 import styles from './styles.module.css';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { RouterLink } from '../../components/RouterLink';
+import { GenericHtml } from '../../components/GenericHtml';
 
 export function History() {
+  const { state } = useTaskContext();
+
   return (
     <MainTemplate>
       <Container>
@@ -24,32 +29,46 @@ export function History() {
       </Container>
 
       <Container>
-        <div className={styles.responsiveTable}>
-          <table>
-            <thead>
-              <tr>
-                <th>Tarefa</th>
-                <th>Duração</th>
-                <th>Data</th>
-                <th>Status</th>
-                <th>Tipo</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Array.from({ length: 20 }).map((_, index) => {
-                return (
-                  <tr key={index}>
-                    <td>Estudar</td>
-                    <td>25min</td>
-                    <td>20/04/2025 08:00</td>
-                    <td>Completa</td>
-                    <td>Foco</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        {state.tasks.length == 0 && (
+          <GenericHtml>
+            <span>Nothing to show here. </span>
+            <span>
+              How about starting{' '}
+              <b>
+                <RouterLink href='/'>a new task</RouterLink>?
+              </b>
+            </span>
+          </GenericHtml>
+        )}
+
+        {state.tasks.length != 0 && (
+          <div className={styles.responsiveTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Tarefa</th>
+                  <th>Duração</th>
+                  <th>Data</th>
+                  <th>Status</th>
+                  <th>Tipo</th>
+                </tr>
+              </thead>
+              <tbody>
+                {state.tasks.map(task => {
+                  return (
+                    <tr key={task.id}>
+                      <td>{task.name}</td>
+                      <td>{task.duration} minute(s)</td>
+                      <td>{new Date(task.startDate).toISOString()}</td>
+                      <td>Completa</td>
+                      <td>Foco</td>
+                    </tr>
+                  ); //---TODO----
+                })}
+              </tbody>
+            </table>
+          </div>
+        )}
       </Container>
     </MainTemplate>
   );
