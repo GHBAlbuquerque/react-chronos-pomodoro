7d7aa59812cc9fcecc0b4ae0dbd9f2fc894b70f2
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-06-29 21:14:53 -0300

    feature: validates values on settings form

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,8 +4,50 @@ import { DefaultButton } from '../../components/DefaultButton';
 import { DefaultInput } from '../../components/DefaultInput';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { useRef } from 'react';
+import { showMessage } from '../../adapters/showMessage';
 
 export function Settings() {
+  const { state, dispatch } = useTaskContext();
+  const workTimeInputRef = useRef<HTMLInputElement>(null);
+  const shortRestTimeInputRef = useRef<HTMLInputElement>(null);
+  const longRestTimeInputRef = useRef<HTMLInputElement>(null);
+
+  const formErrors: string[] = [];
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const workTimeValue = Number(workTimeInputRef.current?.value);
+    const shortRestTimeValue = Number(shortRestTimeInputRef.current?.value);
+    const longRestTimeValue = Number(longRestTimeInputRef.current?.value);
+
+    if (
+      isNaN(workTimeValue) ||
+      isNaN(shortRestTimeValue) ||
+      isNaN(longRestTimeValue)
+    ) {
+      formErrors.push('Please use only numbers.');
+    }
+
+    if (workTimeValue < 1 || workTimeValue > 60) {
+      formErrors.push('Use values between 1 and 60 for focus');
+    }
+
+    if (shortRestTimeValue < 1 || shortRestTimeValue > 15) {
+      formErrors.push('Use values between 1 and 15 for short rest');
+
+      if (longRestTimeValue < 1 || longRestTimeValue > 30) {
+        formErrors.push('Use values between 1 and 30 for long rest');
+      }
+
+      if (formErrors.length > 0) {
+        formErrors.forEach(error => showMessage.error(error));
+      }
+    }
+  }
+
   return (
     <MainTemplate>
       <Container>
@@ -14,26 +56,37 @@ export function Settings() {
 
       <Container>
         <p style={{ textAlign: 'center', fontSize: '1.4rem' }}>
-          Modifique as configurações para tempo de foco, descanso curso e
-          descanso longo.
+          Modify settings for focus, short rest and long rest in <b>minutes</b>.
         </p>
       </Container>
 
       <Container>
-        <form action='' className='form'>
+        <form onSubmit={handleSubmit} action='' className='form'>
           <div className='formRow'>
-            <DefaultInput id='workTime' labelText='Focus'></DefaultInput>
+            <DefaultInput
+              id='workTime'
+              labelText='Focus'
+              ref={workTimeInputRef}
+              defaultValue={state.config.workTime}
+              type='number'
+            ></DefaultInput>
           </div>
           <div className='formRow'>
             <DefaultInput
               id='shortBreakTime'
               labelText='Short break'
+              ref={shortRestTimeInputRef}
+              defaultValue={state.config.shortBreakTime}
+              type='number'
             ></DefaultInput>
           </div>
           <div className='formRow'>
             <DefaultInput
               id='longBreakTime'
               labelText='Long break'
+              ref={longRestTimeInputRef}
+              defaultValue={state.config.longBreakTime}
+              type='number'
             ></DefaultInput>
           </div>
           <div className='formRow'>
